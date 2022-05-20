1e9b1893f43edc95860684d25e3c8957a6dc4238
Harry Kurn <alternate-se7en@pm.me>
2022-05-20 20:33:33 +0700

    scripts/sh: Minor fixes [staging]
    Use `case` switch for those whose condition is simple, and rollback
    `grep` to query media player's `stat` for perfection.
    
    Signed-off-by: Harry Kurn <alternate-se7en@pm.me>

--- .scripts/music-controller.sh
+++ .scripts/music-controller.sh
@@ -9,30 +9,27 @@ exec 2>/dev/null
 
 MUSIC_PLAYER="$(joyd_launch_apps -g music_player)"
 
-if [ "$MUSIC_PLAYER" = 'mpd' ]; then
-    PREV="mpc -p ${CHK_MPD_PORT} prev -q"
-    NEXT="mpc -p ${CHK_MPD_PORT} next -q"
-    STOP="mpc -p ${CHK_MPD_PORT} stop -q"
-    TOGG="mpc -p ${CHK_MPD_PORT} toggle -q"
-    STAT="$(mpc -p ${CHK_MPD_PORT} status)" \
-    STAT="${STAT%%*\[playing\]\ *}"
-    TITL="$(mpc -p ${CHK_MPD_PORT} -f '[%title%|%file%]' current)"
-elif [ "$MUSIC_PLAYER" = 'spotify' ]; then
-    MP2P='org.mpris.MediaPlayer2.Player'
-    SEND="dbus-send --print-reply --dest=${MP2P%.*}.spotify /org/mpris/MediaPlayer2"
-    PROP="org.freedesktop.DBus.Properties.Get string:${MP2P}"
-    PREV="${SEND} ${MP2P}.Previous"
-    NEXT="${SEND} ${MP2P}.Next"
-    STOP="${SEND} ${MP2P}.Stop"
-    TOGG="${SEND} ${MP2P}.PlayPause"
-    STAT="$(${SEND} ${PROP} string:PlaybackStatus)" \
-    STAT="${STAT%%*\ \"Playing\"}"
-    TITL="$(${SEND} ${PROP} string:Metadata | grep -m1 -A1 -F '"xesam:title"')" \
-    TITL="${TITL##*string\ \"}" \
-    TITL="${TITL%\"*}"
-else
-    TITL="There's no MPD nor Spotify installed"
-fi
+case "$MUSIC_PLAYER" in
+    mpd    ) PREV="mpc -p ${CHK_MPD_PORT} prev -q"
+             NEXT="mpc -p ${CHK_MPD_PORT} next -q"
+             STOP="mpc -p ${CHK_MPD_PORT} stop -q"
+             TOGG="mpc -p ${CHK_MPD_PORT} toggle -q"
+             STAT="$(mpc -p ${CHK_MPD_PORT} status | grep -m1 -F '[playing]')"
+             TITL="$(mpc -p ${CHK_MPD_PORT} -f '[%title%|%file%]' current)"
+    ;;
+    spotify) MP2P='org.mpris.MediaPlayer2.Player'
+             SEND="dbus-send --print-reply --dest=${MP2P%.*}.spotify /org/mpris/MediaPlayer2"
+             PROP="org.freedesktop.DBus.Properties.Get string:${MP2P}"
+             PREV="${SEND} ${MP2P}.Previous"
+             NEXT="${SEND} ${MP2P}.Next"
+             STOP="${SEND} ${MP2P}.Stop"
+             TOGG="${SEND} ${MP2P}.PlayPause"
+             STAT="$(${SEND} ${PROP} string:PlaybackStatus | grep -m1 -F '"Playing"')"
+             TITL="$(${SEND} ${PROP} string:Metadata | grep -m1 -A1 -F '"xesam:title"')" \
+             TITL="${TITL##*string\ \"}" \
+             TITL="${TITL%\"*}"
+    ;;
+esac
 
 case "${1}" in
     prev) ${PREV} >&2 &
@@ -45,11 +42,11 @@ case "${1}" in
     ;;
     sta*) echo "$STAT"
     ;;
-    tit*) echo "$TITL"
+    tit*) echo "${TITL:-There's no MPD nor Spotify installed}"
     ;;
-    ico*) [ -n "$STAT" ] && echo '' || echo ''
+    ico*) [ -n "$STAT" ] && echo '' || echo ''
     ;;
-    swi*) [ -n "$STAT" ] || ${TOGG} >&2 &
+    swi*) [ -z "$STAT" ] || ${TOGG} >&2 &
 
           for M in mpd spotify; do
               [ "$MUSIC_PLAYER" != "$M" ] || continue
