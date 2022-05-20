c7924e5c549a10978523ca3423138a9bea20bd54
Harry Kurn <alternate-se7en@pm.me>
2022-05-20 20:34:48 +0700

    joyd/sh: Minor updates
    * Added rofi configuration synchronizer in `joyd_layout_set`.
    
    Signed-off-by: Harry Kurn <alternate-se7en@pm.me>

--- .config/openbox/joyful-desktop/layout-set.sh
+++ .config/openbox/joyful-desktop/layout-set.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# Setup Openbox margins, dunst, and rofi (not implemented yet).
+# Setup Openbox margins, dunst, and rofi.
 # https://github.com/owl4ce/dotfiles
 
 # SPDX-License-Identifier: ISC
@@ -17,6 +17,14 @@ case "$CHK_MODE" in
                         sed -e '/^origin[ ]*/s|[^[ ]*]*$|top-right|' \
                             -e '/^offset[ ]*/s|[^[ ]*]*$|25x50|' \
                             -s -i "$DUNST_DIR"/*.'artistic.dunstrc'
+                        # Synchronize rofi configuration for Artistic Mode.
+                        sed -e '/^[ ]*exts-window-height/s|:[ ]*.*;$|:               100%;|' \
+                            -e '/^[ ]*exts-window-location/s|:[ ]*.*;$|:             east;|' \
+                            -e '/^[ ]*exts-window-x-offset/s|:[ ]*.*;$|:             0px;|' \
+                            -e '/^[ ]*exts-window-y-offset/s|:[ ]*.*;$|:             0px;|' \
+                            -e '/^[ ]*exts-window-border-radius/s|:[ ]*.*;$|:        0px 0px 0px 0px;|' \
+                            -e '/^[ ]*exts-message-border-radius/s|:[ ]*.*;$|:       8px 8px 8px 8px;|' \
+                            -i "$ROFI_SHARED_CONFIG"
                         # Synchronize Openbox margins configuration for Artistic Mode.
                         sed -e "/^[ ]*<top>/s|>[0-9]*<|>${OB_MARGIN}<|" \
                             -e "/^[ ]*<bottom>/s|>[0-9]*<|>${OB_MARGIN}<|" \
@@ -28,6 +36,14 @@ case "$CHK_MODE" in
                         sed -e '/^origin[ ]*/s|[^[ ]*]*$|top-left|' \
                             -e '/^offset[ ]*/s|[^[ ]*]*$|25x50|' \
                             -s -i "$DUNST_DIR"/*.'artistic.dunstrc'
+                        # Synchronize rofi configuration for Artistic Mode.
+                        sed -e '/^[ ]*exts-window-height/s|:[ ]*.*;$|:               100%;|' \
+                            -e '/^[ ]*exts-window-location/s|:[ ]*.*;$|:             west;|' \
+                            -e '/^[ ]*exts-window-x-offset/s|:[ ]*.*;$|:             0px;|' \
+                            -e '/^[ ]*exts-window-y-offset/s|:[ ]*.*;$|:             0px;|' \
+                            -e '/^[ ]*exts-window-border-radius/s|:[ ]*.*;$|:        0px 0px 0px 0px;|' \
+                            -e '/^[ ]*exts-message-border-radius/s|:[ ]*.*;$|:       8px 8px 8px 8px;|' \
+                            -i "$ROFI_SHARED_CONFIG"
                         # Synchronize Openbox margins configuration for Artistic Mode.
                         sed -e "/^[ ]*<top>/s|>[0-9]*<|>${OB_MARGIN}<|" \
                             -e "/^[ ]*<bottom>/s|>[0-9]*<|>${OB_MARGIN}<|" \
@@ -39,6 +55,14 @@ case "$CHK_MODE" in
                         sed -e '/^origin[ ]*/s|[^[ ]*]*$|bottom-right|' \
                             -e '/^offset[ ]*/s|[^[ ]*]*$|25x25|' \
                             -s -i "$DUNST_DIR"/*.'artistic.dunstrc'
+                        # Synchronize rofi configuration for Artistic Mode.
+                        sed -e '/^[ ]*exts-window-height/s|:[ ]*.*;$|:               89.5%;|' \
+                            -e '/^[ ]*exts-window-location/s|:[ ]*.*;$|:             south east;|' \
+                            -e '/^[ ]*exts-window-x-offset/s|:[ ]*.*;$|:             0px;|' \
+                            -e '/^[ ]*exts-window-y-offset/s|:[ ]*.*;$|:             0px;|' \
+                            -e '/^[ ]*exts-window-border-radius/s|:[ ]*.*;$|:        60px 0px 0px 0px;|' \
+                            -e '/^[ ]*exts-message-border-radius/s|:[ ]*.*;$|:       45px 8px 8px 8px;|' \
+                            -i "$ROFI_SHARED_CONFIG"
                         # Synchronize Openbox margins configuration for Artistic Mode.
                         sed -e "/^[ ]*<top>/s|>[0-9]*<|>$((45+OB_MARGIN))<|" \
                             -e "/^[ ]*<bottom>/s|>[0-9]*<|>${OB_MARGIN}<|" \
@@ -50,6 +74,14 @@ case "$CHK_MODE" in
                         sed -e '/^origin[ ]*/s|[^[ ]*]*$|top-right|' \
                             -e '/^offset[ ]*/s|[^[ ]*]*$|25x50|' \
                             -s -i "$DUNST_DIR"/*.'artistic.dunstrc'
+                        # Synchronize rofi configuration for Artistic Mode.
+                        sed -e '/^[ ]*exts-window-height/s|:[ ]*.*;$|:               89.5%;|' \
+                            -e '/^[ ]*exts-window-location/s|:[ ]*.*;$|:             south east;|' \
+                            -e '/^[ ]*exts-window-x-offset/s|:[ ]*.*;$|:             0px;|' \
+                            -e '/^[ ]*exts-window-y-offset/s|:[ ]*.*;$|:             -45px;|' \
+                            -e '/^[ ]*exts-window-border-radius/s|:[ ]*.*;$|:        60px 0px 0px 0px;|' \
+                            -e '/^[ ]*exts-message-border-radius/s|:[ ]*.*;$|:       45px 8px 8px 8px;|' \
+                            -i "$ROFI_SHARED_CONFIG"
                         # Synchronize Openbox margins configuration for Artistic Mode.
                         sed -e "/^[ ]*<top>/s|>[0-9]*<|>${OB_MARGIN}<|" \
                             -e "/^[ ]*<bottom>/s|>[0-9]*<|>$((45+OB_MARGIN))<|" \
@@ -63,6 +95,14 @@ case "$CHK_MODE" in
           sed -e '/^origin[ ]*/s|[^[ ]*]*$|bottom-center|' \
               -e '/^offset[ ]*/s|[^[ ]*]*$|0x60|' \
               -s -i "$DUNST_DIR"/*.'interactive.dunstrc'
+          # Synchronize rofi configuration for Interactive Mode.
+          sed -e '/^[ ]*exts-window-height/s|:[ ]*.*;$|:               89.5%;|' \
+              -e '/^[ ]*exts-window-location/s|:[ ]*.*;$|:             south east;|' \
+              -e '/^[ ]*exts-window-x-offset/s|:[ ]*.*;$|:             0px;|' \
+              -e '/^[ ]*exts-window-y-offset/s|:[ ]*.*;$|:             0px;|' \
+              -e '/^[ ]*exts-window-border-radius/s|:[ ]*.*;$|:        60px 0px 0px 0px;|' \
+              -e '/^[ ]*exts-message-border-radius/s|:[ ]*.*;$|:       45px 8px 8px 8px;|' \
+              -i "$ROFI_SHARED_CONFIG"
           # Synchronize Openbox margins configuration for Interactive Mode.
           sed -e "/^[ ]*<top>/s|>[0-9]*<|>$((30+OB_MARGIN))<|" \
               -e "/^[ ]*<bottom>/s|>[0-9]*<|>${OB_MARGIN}<|" \
