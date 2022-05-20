ff11777fef745ac93eb665588f9d011380c244e0
Harry Kurn <alternate-se7en@pm.me>
2022-05-20 20:34:37 +0700

    mpd/sh: Comments for readability
    Is this enough to explain the steps?
    
    Signed-off-by: Harry Kurn <alternate-se7en@pm.me>

--- .config/mpd/scripts/mpd-notifier.sh
+++ .config/mpd/scripts/mpd-notifier.sh
@@ -11,25 +11,34 @@ export LANG='POSIX'
 exec >/dev/null 2>&1
 . "${HOME}/.joyfuld"
 
+# Ensure `mpd` and `mpc` already installed for safety and performance reasons.
 [ -x "$(command -v mpd)" -a -x "$(command -v mpc)" ] || exec dunstify 'Install `mpd` and `mpc`!' \
                                                                       -h string:synchronous:install-deps \
                                                                       -a joyful_desktop \
                                                                       -u low
 
+# https://gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html#:~:text=expand_aliases
 [ -z "$BASH" ] || shopt -s expand_aliases
 
+# Process as a background task.
 {
+    # Ensure mpd isn't running before.
     ! pidof -s mpd -q || mpd --kill || killall -9 mpd -q
 
+    # Execute mpd with niceness, fallback to fails notification.
     nice -n 1 mpd || mpd || exec dunstify 'Music Player' 'Failed to execute <u>mpd</u>!' \
                                           -h string:synchronous:music-player \
                                           -a joyful_desktop \
                                           -i "$MUSIC_ICON" \
                                           -u low
 
+    # Loop condition of the playing track.
     while :; do
+        # Save the last played track.
         LAST_PLAYED_TRACK="$(mpc -p "$CHK_MPD_PORT" -f '%file%' current)"
+        # Stop while loop, resume when event changes.
         nice -n 19 mpc -p "$CHK_MPD_PORT" idle || mpc -p "$CHK_MPD_PORT" idle
+        # Send notification when current and saved tracks differ.
         [ "$LAST_PLAYED_TRACK" = "$(mpc -p "$CHK_MPD_PORT" -f '%file%' current)" ] || joyd_mpd_notify_albumart
     done
 } &
--- .config/mpd/scripts/notify.album-art.sh
+++ .config/mpd/scripts/notify.album-art.sh
@@ -11,15 +11,21 @@ export LANG='POSIX'
 exec >/dev/null 2>&1
 . "${HOME}/.joyfuld"
 
+# Ensure `mpd`, `mpc`, and `magick` already installed for safety and performance reasons.
 [ -x "$(command -v mpd)" -a -x "$(command -v mpc)" -a -x "$(command -v magick)" ] || exit ${?}
 
+# Process as a background task.
 {
+    # Get file path, merge with album-name of current track.
     FILE="$(mpc -p "$CHK_MPD_PORT" -f '%file% ########## %album%' current)"
 
+    # Ensure the file path is valid.
     [ -n "${FILE%/*\ #####\ *}" ] || exit ${?}
 
+    # Update $CHK_MPD_MUSIC_DIR if in the mpd configuration contains tilde. This is bug fix.
     [ -n "${CHK_MPD_MUSIC_DIR%%~*}" ] || CHK_MPD_MUSIC_DIR="${HOME}/${CHK_MPD_MUSIC_DIR#~*/}"
 
+    # Get single album-art image.
     read -r ALBUM_COVER <<- EOF
 		$(find "${CHK_MPD_MUSIC_DIR}/${FILE%/*\ #####\ *}/" -maxdepth 1 \
 															-type f \
@@ -27,6 +33,7 @@ exec >/dev/null 2>&1
 		".*/.*\(${FILE##*\ #####\ }\|cover\|folder\|artwork\|front\).*[.]\(jpe?g\|png\|gif\|bmp\)")
 	EOF
 
+    # Rescale and convert album-art image into rounded corners.
     if [ -f "$ALBUM_COVER" ]; then
 
         magick "$ALBUM_COVER" \
@@ -56,8 +63,10 @@ exec >/dev/null 2>&1
         MPD_NOTIFY_AA_IMG=
     fi
 
+    # Get current track artist, merge with title, fallback to file path.
     INFO="$(mpc -p "$CHK_MPD_PORT" -f '%artist% ########## [%title%|%file%]' current)"
 
+    # Send notification with album-art image as raw icon.
     exec dunstify '' "<span size='small'>${INFO%%\ #####\ *}</span>\n${INFO##*\ #####\ }" \
                   -h string:synchronous:ncmpcpp-album-art \
                   -I "${MPD_NOTIFY_AA_IMG:-${MUSIC_ICON}}"
