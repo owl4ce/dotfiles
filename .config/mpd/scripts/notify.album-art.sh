e408320adfa9eb7b3e38493106708f7b0a10004e
Harry Kurn <alternate-se7en@pm.me>
2022-05-20 20:34:26 +0700

    mpd/sh: Minor fixes
    Ensure tilde (equal to ${HOME}) matches globs in the parameter expansion.
    
    Signed-off-by: Harry Kurn <alternate-se7en@pm.me>

--- .config/mpd/scripts/notify.album-art.sh
+++ .config/mpd/scripts/notify.album-art.sh
@@ -18,7 +18,7 @@ exec >/dev/null 2>&1
 
     [ -n "${FILE%/*\ #####\ *}" ] || exit ${?}
 
-    [ -n "${CHK_MPD_MUSIC_DIR%%~*}" ] || CHK_MPD_MUSIC_DIR="${HOME}/${CHK_MPD_MUSIC_DIR#*~/}"
+    [ -n "${CHK_MPD_MUSIC_DIR%%~*}" ] || CHK_MPD_MUSIC_DIR="${HOME}/${CHK_MPD_MUSIC_DIR#~*/}"
 
     read -r ALBUM_COVER <<- EOF
 		$(find "${CHK_MPD_MUSIC_DIR}/${FILE%/*\ #####\ *}/" -maxdepth 1 \
