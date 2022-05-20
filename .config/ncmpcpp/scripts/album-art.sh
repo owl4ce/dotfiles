654af93296d7902b0074c209953a4ae120306ba9
Harry Kurn <alternate-se7en@pm.me>
2022-05-20 20:34:14 +0700

    ncmpcpp/sh: Minor useful updates
    All about performance. Optimize ALBUM_COVER image finder by limiting to
    single directory, improve `magick` processor for saner parameters, etc.
    
    Signed-off-by: Harry Kurn <alternate-se7en@pm.me>

--- .config/ncmpcpp/scripts/album-art.sh
+++ .config/ncmpcpp/scripts/album-art.sh
@@ -22,7 +22,7 @@ w3m()
     echo "${$}" >"$NCMPCPP_AA_PID"
 
     read -r W3M_IMG_DISPLAY <<- EOF
-		$(find ${LIBS_PATH} -type f -path '*/w3m/*' -iname 'w3mim*')
+		$(find ${LIBS_PATH} -type f -path '*/w3m/*' -name 'w3mimg*')
 	EOF
 
     while IFS= read -r P <"$NCMPCPP_AA_PID" && [ "${P:-0}" -eq "${$}" ]; do
@@ -36,7 +36,6 @@ w3m()
             "${W3M_IMG_DISPLAY:-break}" >&2 <<- IMG
 				0;1;0;0;${WIDTH};${WIDTH};;;;;${NCMPCPP_AA_IMG}
 				3;
-				4
 			IMG
 
             WIDTH=
@@ -71,11 +70,10 @@ pixbuf()
     [ -n "${CHK_MPD_MUSIC_DIR%%~*}" ] || CHK_MPD_MUSIC_DIR="${HOME}/${CHK_MPD_MUSIC_DIR#*~/}"
 
     read -r ALBUM_COVER <<- EOF
-		$(find "${CHK_MPD_MUSIC_DIR}/${FILE%/*\ #####\ *}"  -type d \
-															-exec find "{}" -maxdepth 1 \
-																			-type f \
-																			-iregex \
-		".*/.*\(${FILE##*\ #####\ }\|cover\|folder\|artwork\|front\).*[.]\(jpe?g\|png\|gif\|bmp\)" \;)
+		$(find "${CHK_MPD_MUSIC_DIR}/${FILE%/*\ #####\ *}/" -maxdepth 1 \
+															-type f \
+															-iregex \
+		".*/.*\(${FILE##*\ #####\ }\|cover\|folder\|artwork\|front\).*[.]\(jpe?g\|png\|gif\|bmp\)")
 	EOF
 
     if [ -f "$ALBUM_COVER" ]; then
@@ -83,8 +81,6 @@ pixbuf()
         magick "$ALBUM_COVER" \
                -strip \
                -interlace Plane \
-               -sampling-factor 4:2:0 \
-               -define jpeg:dct-method=float \
                -scale 500x500\! \
                -depth 8 \
            '(' -clone 0 \
@@ -101,8 +97,9 @@ pixbuf()
            ')' -alpha off \
                -compose CopyOpacity \
                -composite \
-               -quality 85% \
-        "$NCMPCPP_AA_IMG"
+               -quality 9 \
+        "$NCMPCPP_AA_IMG" \
+        || magick -depth 8 canvas:transparent "PNG8:${NCMPCPP_AA_IMG}"
 
     else
         magick -depth 8 canvas:transparent "PNG8:${NCMPCPP_AA_IMG}"
