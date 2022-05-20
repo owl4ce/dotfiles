3f22a47d108f3eb3d9798a35b0aee33952e013fe
Harry Kurn <alternate-se7en@pm.me>
2022-05-20 20:33:29 +0700

    ncmpcpp/sh: Minor updates [staging]
    Suppress logs and excess output of `ncmpcpp` from "launcher.sh".
    
    Signed-off-by: Harry Kurn <alternate-se7en@pm.me>

--- .config/ncmpcpp/scripts/launcher.sh
+++ .config/ncmpcpp/scripts/launcher.sh
@@ -12,11 +12,11 @@ MUSIC_PLAYER="$(joyd_launch_apps -g music_player)"
 
 if [ "$MUSIC_PLAYER" = 'mpd' ]; then
     case "${1}" in
-        '') exec $(LANG="$SYSTEM_LANG" joyd_launch_apps terminal -e ncmpcpp &)
+        '') exec $(LANG="$SYSTEM_LANG" joyd_launch_apps terminal -e ncmpcpp -q &)
         ;;
-        a*) exec $(LANG="$SYSTEM_LANG" urxvt -g 84x13 -e ncmpcpp -c "${NCMPCPP_DIR}/album-art.config" &)
+        a*) exec $(LANG="$SYSTEM_LANG" urxvt -g 84x13 -e ncmpcpp -c "${NCMPCPP_DIR}/album-art.config" -q &)
         ;;
-        s*) exec $(LANG="$SYSTEM_LANG" urxvt -g 47x18 -e ncmpcpp -c "${NCMPCPP_DIR}/single.album-art.config" &)
+        s*) exec $(LANG="$SYSTEM_LANG" urxvt -g 47x18 -e ncmpcpp -c "${NCMPCPP_DIR}/single.album-art.config" -q &)
         ;;
     esac
 else
