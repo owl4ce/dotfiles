f8334a0d4c7663daf451f166d48ccda7d722b75a
Harry Kurn <alternate-se7en@pm.me>
2022-06-08 20:24:40 +0700

    ob-autostart: Comments changes
    Only preserve the important comments, and improve some of them.
    
    Signed-off-by: Harry Kurn <alternate-se7en@pm.me>

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -14,28 +14,23 @@ exec >/dev/null 2>&1
 # https://gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html#:~:text=expand_aliases
 [ -z "$BASH" ] || shopt -s expand_aliases
 
-# Run URxvt daemon if the user's current preferred terminal emulator is set to `urxvtc`.
 { [ "$(joyd_launch_apps -g terminal)" = 'urxvtc' ] && urxvtd -f -q; } &
 
-# Run your preferred audio server here (init-freedom), there was once a pulseaudio here.
 #{ pidof -s pulseaudio -q || pulseaudio --start --log-target=syslog; } &
 
-# Setup user interface using last applied themes configuration and user's tray programs.
 joyd_toggle_mode apply
 joyd_tray_programs exec
 
-# Run picom compositor (2xBUFFER) to enable shadows, transparency, rounded-corners, etc.
 picom --experimental-backends -b
 
-# Run authentication agent, execute `lxpolkit` or `polkit-gnome-authentication-agent-*`.
 if [ -x "$(command -v lxpolkit)" ]; then
     lxpolkit &
 else
     $(find ${LIBS_PATH} -type f -iname 'polkit-gnome-authentication-agent-*' | sed 1q) &
 fi
 
-# Run session-locker daemon, required by `loginctl` (systemd events) to manage sessions.
 { [ -x "$(command -v xss-lock)" ] && xss-lock -q -l "${JOYD_DIR}/xss-lock-tsl.sh"; } &
 
-# Run Music Player Daemon with album-art notification-sender whenever track was changed.
 joyd_mpd_notifier
+
+# Any additions should be added below.
